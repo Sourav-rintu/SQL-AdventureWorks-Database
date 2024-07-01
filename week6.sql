36c3aa46a7c3e1e1ba7dd2927df2a2c07e26937e
Sourav Bhattacharya <117636546+Sourav-rintu@users.noreply.github.com>
2024-07-01 22:40:13 +0530

    Create week6.sql

---
+++ week6.sql
@@ -0,0 +1,22 @@
+1 Combine two tables 
+link: https://leetcode.com/submissions/detail/1305039343/
+2 Employees earning more than their managers
+link: https://leetcode.com/submissions/detail/1305052089/
+3 Duplicate Details
+link: https://leetcode.com/submissions/detail/1305054905/
+4 Customers who never orders
+link: https://leetcode.com/submissions/detail/1305060612/
+5 Deletes Duplicate emails
+link: https://leetcode.com/submissions/detail/1305063866/
+6 Rising Temparature 
+link: https://leetcode.com/submissions/detail/1305066790/
+7 Employee Bonus
+Link: https://leetcode.com/submissions/detail/1305076179/
+8 Find Customers Referee
+link: https://leetcode.com/submissions/detail/1305077186/
+9 Customers placing largest number of orders
+Link: https://leetcode.com/submissions/detail/1305079000/
+10 Big Countries
+Link: https://leetcode.com/submissions/detail/1305079686/
+11 Classes More than 5 students
+Link: https://leetcode.com/submissions/detail/1305080359/
